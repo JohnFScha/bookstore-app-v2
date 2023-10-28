cc04e95c559638c915d91d51970e89ca10279c8b
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-10-28 01:41:17 -0300

    test-2

--- next.config.js
+++ next.config.js
@@ -12,16 +12,6 @@ const nextConfig = {
   },
   experimental: {
     serverActions: true,
-    esmExternals: "loose", // <-- add this
-    serverComponentsExternalPackages: ["mongoose"] // <-- and this
-  },
-  // and the following to enable top-level await support for Webpack
-  webpack: (config) => {
-    config.experiments = {
-      layers: true,
-      topLevelAwait: true
-    };
-    return config;
   },
 }
 
