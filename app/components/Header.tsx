412fa7bc5d451a2667e56431c15dffe77891cd73
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-10-28 18:23:04 -0300

    header styles for mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,8 +17,8 @@ const links = [
 
 export default function Header() {
   return (
-    <header className='flex lg:flex-nowrap lg:justify-between md:justify-between md:p-3 xs:p-5 border-b-2 rounded-b-xl shadow-md shadow-white xs:flex-wrap xs:justify-center xs:gap-5'>
-      <div className='flex md:justify-start lg:justify-start md:w-auto xs:w-full xs:justify-between items-center gap-2'>
+    <header className='flex lg:flex-nowrap lg:justify-between md:justify-between md:p-3 xs:p-5 border-b-2 rounded-b-xl shadow-md shadow-white  xs:justify-center xs:gap-5'>
+      <div className='flex md:flex-row xs:flex-row-reverse md:justify-start lg:justify-start md:w-auto xs:w-full xs:justify-between items-center gap-2'>
         <Image src='/next.svg' alt='Bookstore app' width={80} height={80} className='bg-white p-2 rounded-lg' />
         <span className='md:inline xs:hidden'>|</span>
         <p className='italic text-xl'>Bookstore App</p>
